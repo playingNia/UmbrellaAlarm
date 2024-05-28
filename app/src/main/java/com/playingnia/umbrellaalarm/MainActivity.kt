e418595d045f825a2ef6b9c36541a10ef5f536ae
playingNia <playingnia@gmail.com>
2024-05-28 23:08:49 +0900

    🎈 GPS 권한 설정

--- app/src/main/java/com/playingnia/umbrellaalarm/MainActivity.kt
+++ app/src/main/java/com/playingnia/umbrellaalarm/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.playingnia.umbrellaalarm
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.location.LocationServices
 import com.playingnia.umbrellaalarm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val ACCESS_FINE_LOCATION = android.Manifest.permission.ACCESS_FINE_LOCATION
+    private val ACCESS_COARSE_LOCATION = android.Manifest.permission.ACCESS_COARSE_LOCATION
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions()
+        } else {
+            getLocation(binding.textTitle)
+        }
 
 
 //        enableEdgeToEdge()
@@ -23,5 +40,41 @@ class MainActivity : AppCompatActivity() {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
 //        }
+    }
+
+    private fun requestPermissions() {
+        val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            when {
+                permissions[ACCESS_FINE_LOCATION] == true -> {
+                    getLocation(binding.textTitle)
+                }
+
+                permissions[ACCESS_COARSE_LOCATION] == true -> {
+                    getLocation(binding.textTitle)
+                }
+
+                else -> {
+                    Toast.makeText(this, resources.getString(R.string.permisson_denied), Toast.LENGTH_LONG).show()
+                    moveTaskToBack(true)
+                    finish()
+                    android.os.Process.killProcess(android.os.Process.myPid())
+                }
+            }
+        }
+
+        requestPermissionLauncher.launch(arrayOf(ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION))
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation(textView: TextView) {
+        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { success: Location? ->
+            success?.let { location ->
+                textView.text = "${location.latitude}, ${location.longitude}"
+            }}
+            .addOnFailureListener { fail ->
+                textView.text = fail.localizedMessage
+        }
     }
 }
